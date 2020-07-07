7355cc681434ebc0961ebffffd9ee5ca381994b0
nook <nook.ittipon@gmail.com>
2020-07-08 00:08:15 +0700

    finish api demo

--- abc-app/app.ts
+++ abc-app/app.ts
@@ -1,5 +1,5 @@
 import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
-
+import {get_all_books, get_book, create_book, delete_book } from './controllers/bookController.ts'
 const app = new Application()
 
 /** Static files */
@@ -9,6 +9,11 @@ app.static('/', './public')
 app.get('/', async (ctx: Context) => {
     await ctx.file('./public/index.html')
 })
+
+app.get('/books', get_all_books)
+.get('/books/:id', get_book)
+.post('/books', create_book)
+.delete('/books/:id', delete_book)
 
 //listen to port
 app.start({
